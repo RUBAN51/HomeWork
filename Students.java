7536ebcd0031348b52f36de99b51168493e31e5a
RUBAN51 <136977090+RUBAN51@users.noreply.github.com>
2023-08-26 12:57:00 +0530

    Add files via upload
    Student cant see fees but teacher can see all the variables

---
+++ Students.java
@@ -0,0 +1,14 @@
+package homeworks;
+
+public class Students extends School{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Students s = new Students();
+		s.name="Velammal";
+		s.history="sixyears";
+		s.branch="chennai";		
+		
+	}
+
+}
---
+++ Teachers.java
@@ -0,0 +1,23 @@
+package homeworks;
+class School{
+	String name;
+	String history;
+	String branch;
+	private int fees;
+	int getValue() {
+		return fees;
+	}
+	
+}
+public class Teachers extends School{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Teachers t = new Teachers();
+		t.name="Velammal";
+		t.history="sixyears";
+		t.branch="chennai";
+		System.out.println(t.getValue());
+	}
+
+}
